2e7093eedffa8f54f135738a8ee8f58628016d51
Aniket Pandit <aniketpandit9206@gmail.com>
2023-05-27 12:55:21 +0530

    adding files

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+
+global.foodData = require('./db')(function call(err, data, Catdata) {
+  // console.log(data)
+  if(err) console.log(err);
+  global.foodData = data;
+  global.foodCategory = Catdata;
+})
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 5000
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json())
+app.use(cors({
+  origin : "*"
+}))
+
+app.use('/api/auth', require('./Routes/Auth'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
